604c8e4e0acf00532ea73523455c1ce6707ff75e
sthuthi200 <4al24cs200@aiet.org.in>
2026-05-15 14:30:05 +0530

    Add files via upload

--- HelloSwing.java
+++ HelloSwing.java
@@ -1,30 +1,17 @@
-package mypackage;
+package swingexamples;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class HelloSwing {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello Program");
+        JLabel label = new JLabel("Hello! VI C , Welcome to Swing Programming", JLabel.CENTER);
 
-        // Create JFrame
-        JFrame frame = new JFrame("Swing Hello Program");
-
-        // Create JLabel with message
-        JLabel label = new JLabel("Hello! VI C , Welcome to Swing Programming");
-
-        // Set font (Plain, Size 32)
         label.setFont(new Font("Arial", Font.PLAIN, 32));
-
-        // Set text color to Blue
         label.setForeground(Color.BLUE);
 
-        // Align text to center
-        label.setHorizontalAlignment(JLabel.CENTER);
-
-        // Add label to frame
         frame.add(label);
-
-        // Frame settings
         frame.setSize(800, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
