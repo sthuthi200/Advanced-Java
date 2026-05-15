5b24c6a00246ae041d9b6cdf809fb4a3db6c00d7
sthuthi200 <4al24cs200@aiet.org.in>
2026-05-15 15:01:15 +0530

    Add files via upload

---
+++ CMYTabbedPane.java
@@ -0,0 +1,29 @@
+package swingexamples;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CMYTabbedPane {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CMY Tabbed Pane");
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel cyanPanel = new JPanel();
+        cyanPanel.setBackground(Color.CYAN);
+
+        JPanel magentaPanel = new JPanel();
+        magentaPanel.setBackground(Color.MAGENTA);
+
+        JPanel yellowPanel = new JPanel();
+        yellowPanel.setBackground(Color.YELLOW);
+
+        tabbedPane.addTab("Cyan", cyanPanel);
+        tabbedPane.addTab("Magenta", magentaPanel);
+        tabbedPane.addTab("Yellow", yellowPanel);
+
+        frame.add(tabbedPane);
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
